9f31053aeefa067180d95cd2fccd46fcb5d8aec9
carteseb000 <50633893+carteseb000@users.noreply.github.com>
2019-05-21 10:19:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var Player = function(x,y,size,speed) {
   this.y = y;
   this.size = size; 
   this.speed = speed;
+  update();
    this.update = function() {
       ellipse(this.x,this.y,this.size,this.size);
    };
@@ -13,3 +14,10 @@ var player = new Player(100,100,9,3);
 draw = function() {
    player.update();
 };
+
+function update() {
+   if(key.a) {move(-5, 0);}
+   if(key.d) {move(5 , 0);}
+   if(key.w) {move(0, -5);}
+   if(key.s) {move(0 , 5);}
+}
